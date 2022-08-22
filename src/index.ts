62a88ef6c0221f32bc8fda47a10a953d99fd3d79
Robson Carvalho <robson01jc@gmail.com>
2022-08-22 16:42:21 -0300

    event system

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ age: 9999 });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
